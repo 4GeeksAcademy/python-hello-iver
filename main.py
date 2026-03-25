197b6a0b5931d4ca56e4e6d78853e99f711d8b75
Hubert Carrasset <hcarrassets@gmail.com>
2026-03-25 16:24:27 +0000

    excusas

--- main.py
+++ main.py
@@ -1,1 +1,17 @@
-print("Hello World")
+import random
+
+who = ['the dog ','my granma ','his turtle ','my bird ']
+what = ['eat ','pissed ','crushed ','broked ']
+when = ['before the class','right in time','when I finished','during my lunch','while I was praying']
+
+def generate_excuse():
+    excuse = "" 
+    who_random = random.choice(who)
+    excuse += who_random 
+    what_random = random.choice(what)
+    excuse += what_random  
+    when_random = random.choice(when)
+    excuse += when_random 
+    print(excuse)
+
+generate_excuse() 
